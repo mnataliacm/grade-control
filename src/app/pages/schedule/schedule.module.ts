626f6b473f942806581c78fb7cff5b1fca2f9124
Natalia CM <mnacamu@gmail.com>
2022-12-03 02:24:10 +0100

    añado fullcalendar falta configurar y asignar

--- src/app/pages/schedule/schedule.module.ts
+++ src/app/pages/schedule/schedule.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
 import { SchedulePageRoutingModule } from './schedule-routing.module';
-
 import { SchedulePage } from './schedule.page';
+import { CoreModule } from 'src/app/core/core.module';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import scrollgridPlugin from '@fullcalendar/scrollgrid';
+import daygridPlugin from '@fullcalendar/daygrid';
+import timegridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+FullCalendarModule.registerPlugins([
+  scrollgridPlugin,
+  daygridPlugin,
+  timegridPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    SchedulePageRoutingModule
+    CoreModule,
+    SchedulePageRoutingModule,
+    FullCalendarModule
   ],
   declarations: [SchedulePage]
 })
--- src/app/pages/schedule/schedule.page.ts
+++ src/app/pages/schedule/schedule.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import { StudentService } from 'src/app/core';
+import { TaskService } from 'src/app/core/services/task.service';
 
 @Component({
   selector: 'app-schedule',
@@ -7,9 +10,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchedulePage implements OnInit {
 
-  constructor() { }
+  public calendarOptions: CalendarOptions = this.initCalendar();
 
-  ngOnInit() {
+  constructor(
+    //private assignmentsService:AssignmentService,
+    private studentSvc:StudentService,
+    private tasksSvc:TaskService,
+    private containerRef: ViewContainerRef
+  ) {
+    // this.assignmentsService._assigns$.subscribe((tasks)=>{     
+    //   this.calendarOptions = {
+    //     locale:esLocale,
+    //     initialView: 'timeGridDay',
+    //     height: 'auto',
+    //     slotDuration: '00:30:00',
+    //     slotLabelInterval: '00:30',
+    //     eventOverlap:false,
+    //     contentHeight:'auto',
+    //     eventChange:(event)=>{
+    //       console.log(event.event.start);
+    //       console.log(event.event.extendedProps.assignment.dateTime)         
+    //       var assignment = {...event.event.extendedProps.assignment};
+    //       assignment.dateTime = moment(event.event.start).toISOString();
+    //       this.assignmentsService.updateAssignment(assignment);         
+    //     },
+    //     editable:true,
+    //     events: tasks.map(a=>{
+    //       var task = this.tasksService.getTaskById(a.taskId);
+    //       return {
+    //         "title":task.name, 
+    //         "start":moment(a.dateTime).toISOString(), 
+    //         "end":moment(a.dateTime).add(task.time, 'seconds').toISOString(),
+    //         "assignment":a
+    //       };
+    //     }),
+    //     eventContent:(arg)=>{
+    //       var comp:ComponentRef<AssignmentScheduleComponent> = this.containerRef.createComponent(AssignmentScheduleComponent);
+    //       comp.instance.assign = arg.event.extendedProps.assignment;
+    //       return { domNodes: [comp.location.nativeElement] }
+          
+    //     }
+    //  };     
+    //});
+  }
+
+  public ngOnInit(): void {
+    // HACK Rerender the calendar and correctly display it
+     setTimeout(() => {
+       this.calendarOptions.footerToolbar = false;
+     }, 300);
+  }
+
+   private initCalendar(): CalendarOptions {
+    return {
+      initialView: 'timeGridWeek',
+      height: 'auto',
+      slotDuration: '06:00:00',
+      slotLabelInterval: '01:00',
+      editable:true,
+      events: [
+    ],
+    };
   }
 
 }
