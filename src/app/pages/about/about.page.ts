d255b64bb64811561eb1d397226b3bfc6be04f27
Natalia CM <mnacamu@gmail.com>
2022-12-15 23:55:39 +0100

    listo, pendiente de mejoras

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { isLowResolution as lowres} from 'src/app/utils/screen.utils';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+
+  isLowResolution:()=>boolean = lowres;
 
   constructor() { }
 
